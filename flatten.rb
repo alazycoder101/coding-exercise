dc7817373f40ea8b827a0bed333872f1930f2bf7
lazycoder <alazycoder101@gmail.com>
2021-11-29 18:50:37 +0000

    flatten with recusive solution

---
+++ flatten.rb
@@ -0,0 +1,14 @@
+class Array
+  def flatten
+    self.inject([]) do |result, element|
+      if element.is_a?(Array)
+        result + element.flatten
+      else
+        result << element
+      end
+    end
+  end
+end
+
+test = [1, 2, [3, 4, [5, 6], 7], 8]
+p test.flatten
