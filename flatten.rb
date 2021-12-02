8598ca955f8698d2daf46d4a244e7b078e99c349
lazycoder <alazycoder101@gmail.com>
2021-12-02 16:17:22 +0000

    add more tests

--- flatten.rb
+++ flatten.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Array
 class Array
   def flatten
     inject([]) do |result, element|
@@ -12,5 +13,17 @@ class Array
   end
 end
 
+def assert_equal(expected, actual)
+  puts "Expected: #{expected.inspect}, got: #{actual.inspect}" and exit 1 unless expected == actual
+
+  true
+end
+
 test = [1, 2, [3, 4, [5, 6], 7], 8]
 p test.flatten
+
+assert_equal([1, 2], [1, 2].flatten)
+assert_equal([], [].flatten)
+assert_equal([1, 2], [[[[1, 2]]]].flatten)
+assert_equal([1, 2, nil], [[], [1, 2, nil], [], []].flatten)
+assert_equal([1, 2, 3, 4, 5, 6, 7, 8], test.flatten)
